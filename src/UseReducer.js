d9fc2750a2c0bbf005a007e7d4a7cdc7a8db4693
ShineTiger <shinetiger.dev@gmail.com>
2022-07-13 16:57:17 +0900

    useReducer가 만든 state와 reducer 함수

--- src/UseReducer.js
+++ src/UseReducer.js
@@ -10,12 +10,19 @@ action - 요구의 내용
 import { useState, useReducer } from "react";
 
 function UseReducer() {
+  //첫번째는 state, 두번째는 action(요구의 내용)을 받는다.
+  const reducer = (state, action) => {};
+
   const [number, setNumber] = useState(0);
+
+  //useReducer는 배열을 반환해준다. (useState와 비슷)
+  //useReducer가 만든 state인 money는 reducer 함수로만 수정이된다. 그리고 reducer는 useReducer의 인자가된다. reducer를 통해서 state를 수정하고 싶을 때 dispatch를 부른다.
+  const [money, dispatch] = useReducer(reducer, 0); //첫번째 매개변수는 reducer, 두번째인자는 money의 초기값
 
   return (
     <div>
       <h2>useReducer 은행 웰컴</h2>
-      <p>잔고 ?원</p>
+      <p>잔고 {money}원</p>
       <input
         type="number"
         value={number}
